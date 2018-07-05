866305bd8a3b5eff1d992f7fbf6a0f076f105ca2
James Ransome <jamesransome@msn.com>
2018-07-05 23:16:33 +0100

    add models

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const persistence = require('./src/persistence')
 let mainWindow
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 1200, height: 900 })
 
   mainWindow.loadFile('index.html')
 
@@ -18,13 +18,9 @@ const createWindow = () => {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,13 +1,12 @@
-const { Model } = require('objection')
-const sqlite3 = require('sqlite3').verbose()
-
 const dbConfig = require('../../knexfile')
 
+const sqlite3 = require('sqlite3').verbose()
 new sqlite3.Database(dbConfig.connection.filename)
 
 const db = require('knex')(dbConfig)
 db.migrate.latest()
 
+const { Model } = require('objection')
 Model.knex(db)
 
 module.exports = Model
