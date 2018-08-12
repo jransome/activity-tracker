d01c911602a6460a4f7cb13d1f7edd13639750ab
James Ransome <jamesransome@msn.com>
2018-08-12 18:36:56 +0100

    refactor ProcessListener into a class

--- src/ProcessListener/index.js
+++ src/ProcessListener/index.js
@@ -1,35 +1,62 @@
-const { spawn } = require("child_process");
+const { spawn } = require('child_process');
+const { EventEmitter } = require('events');
 
-const startEventIdentifier = 'startevent'
-const stopEventIdentifier = 'stopevent'
+class ProcessListener extends EventEmitter {
+  constructor() {
+    super();
+    const encoding = 'utf8'; // encoding for strings not buffers (as is default)
+    const startEventIdentifier = 'startevent';
+    const stopEventIdentifier = 'stopevent';
+    const psScriptsDir = './src/powershell';
+    const psScriptArgs = `-StartEventIdentifier ${startEventIdentifier} -StopEventIdentifier ${stopEventIdentifier}`;
+    const registerEventsScript = `${psScriptsDir}/register-events.ps1 ${psScriptArgs}\n`;
+    this._unregisterEventsScript = `${psScriptsDir}/unregister-events.ps1 ${psScriptArgs}\n`;
 
-const psScriptsDir = './src/powershell';
-const psScriptArgs = `-StartEventIdentifier ${startEventIdentifier} -StopEventIdentifier ${stopEventIdentifier}`
-const registerEventsScript = `${psScriptsDir}/register-events.ps1 ${psScriptArgs}\n`;
-const unregisterEventsScript = `${psScriptsDir}/unregister-events.ps1 ${psScriptArgs}\n`;
+    const args = ['-ExecutionPolicy', 'Unrestricted', '-NoLogo', '-NoExit', '-InputFormat', 'Text', '-Command', '-'];
+    this._psProc = spawn('powershell.exe', args);
 
-const args = ['-ExecutionPolicy', 'Unrestricted', '-NoLogo', '-NoExit', '-InputFormat', 'Text', '-Command', '-'];
+    if (!this._psProc.pid) throw new Error("Powershell child process did not start");
 
-const child = spawn('powershell.exe', args);
+    this._psProc.on('error', error => {
+      throw new Error("ERROR on Powershell child process: " + error);
+    });
 
-child.stdin.setEncoding('utf-8');
+    this._psProc.stdin.setDefaultEncoding(encoding);
+    this._psProc.stdout.setEncoding(encoding);
+    this._psProc.stderr.setEncoding(encoding);
 
-child.stdin.write(registerEventsScript);
+    this._psProc.stdin.write(registerEventsScript, () => this.emit('Ready'));
 
-setTimeout(function () {
-  child.stdin.write(unregisterEventsScript);
-}, 10000)
+    this._psProc.stdout.on('data', (data) => {
+      this.emit('process-event', data);
+    });
+    
+    this._psProc.stderr.on('data', (data) => {
+      this.emit('powershell-error', data);
+    });
+  }
+  
+  stop() {
+    this._psProc.stdin.write(this._unregisterEventsScript, () => {
+      this._psProc.stdin.end(); 
+      console.log('Stopped listening')
+    });
+  }
+}
 
-child.stdout.on("data", function (data) {
-  console.log("Powershell Data: " + data);
-});
+export default ProcessListener;
 
-child.stderr.on("data", function (data) {
-  console.log("Powershell Errors: " + data);
-});
+// const listener = new ProcessListener();
 
-child.on("exit", function () {
-  console.log("Powershell Script finished");
-});
+// listener.on('process-event', (data) => {
+//   console.log("Process event: " + data);
+// });
 
-// child.stdin.end(); //end input
+// listener.on('powershell-error', (data) => {
+//   console.log("Powershell Error: " + data);
+// });
+
+// setTimeout(() => {
+//   console.log('stopping listener')
+//   listener.stop();
+// }, 10000);
