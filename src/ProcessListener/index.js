286cf462805c8f12f61c7b5d42f995fde903324e
James Ransome <jamesransome@msn.com>
2018-08-13 19:26:14 +0100

    output only JSON events from ProcessListener

--- src/ProcessListener/index.js
+++ src/ProcessListener/index.js
@@ -1,62 +1,68 @@
-const { spawn } = require('child_process');
-const { EventEmitter } = require('events');
+const { spawn } = require('child_process')
+const { EventEmitter } = require('events')
 
 class ProcessListener extends EventEmitter {
   constructor() {
-    super();
-    const encoding = 'utf8'; // encoding for strings not buffers (as is default)
-    const startEventIdentifier = 'startevent';
-    const stopEventIdentifier = 'stopevent';
-    const psScriptsDir = './src/powershell';
-    const psScriptArgs = `-StartEventIdentifier ${startEventIdentifier} -StopEventIdentifier ${stopEventIdentifier}`;
-    const registerEventsScript = `${psScriptsDir}/register-events.ps1 ${psScriptArgs}\n`;
-    this._unregisterEventsScript = `${psScriptsDir}/unregister-events.ps1 ${psScriptArgs}\n`;
+    super()
+    const encoding = 'utf8' // encoding for strings not buffers (as is default)
+    const startEventIdentifier = 'startevent'
+    const stopEventIdentifier = 'stopevent'
+    const psScriptsDir = './src/powershell'
+    const psScriptArgs = `-StartEventIdentifier ${startEventIdentifier} -StopEventIdentifier ${stopEventIdentifier}`
+    const registerEventsScript = `${psScriptsDir}/register-events.ps1 ${psScriptArgs}\n`
+    this._unregisterEventsScript = `${psScriptsDir}/unregister-events.ps1 ${psScriptArgs}\n`
 
-    const args = ['-ExecutionPolicy', 'Unrestricted', '-NoLogo', '-NoExit', '-InputFormat', 'Text', '-Command', '-'];
-    this._psProc = spawn('powershell.exe', args);
+    const args = ['-ExecutionPolicy', 'Unrestricted', '-NoLogo', '-NoExit', '-InputFormat', 'Text', '-Command', '-']
+    this._psProc = spawn('powershell.exe', args)
 
-    if (!this._psProc.pid) throw new Error("Powershell child process did not start");
+    if (!this._psProc.pid) throw new Error("Powershell child process did not start")
 
     this._psProc.on('error', error => {
-      throw new Error("ERROR on Powershell child process: " + error);
-    });
+      throw new Error("ERROR on Powershell child process: " + error)
+    })
 
-    this._psProc.stdin.setDefaultEncoding(encoding);
-    this._psProc.stdout.setEncoding(encoding);
-    this._psProc.stderr.setEncoding(encoding);
+    this._psProc.stdin.setDefaultEncoding(encoding)
+    this._psProc.stdout.setEncoding(encoding)
+    this._psProc.stderr.setEncoding(encoding)
 
-    this._psProc.stdin.write(registerEventsScript, () => this.emit('Ready'));
+    this._psProc.stdin.write(registerEventsScript, () => this.emit('Ready'))
 
     this._psProc.stdout.on('data', (data) => {
-      this.emit('process-event', data);
-    });
-    
+      try {
+        const event = JSON.parse(data)
+        this.emit('process-event', event)
+      } catch (error) {
+        console.log('Non JSON PS output handled')
+      }
+    })
+
     this._psProc.stderr.on('data', (data) => {
-      this.emit('powershell-error', data);
-    });
+      this.emit('powershell-error', data)
+    })
   }
-  
+
   stop() {
     this._psProc.stdin.write(this._unregisterEventsScript, () => {
-      this._psProc.stdin.end(); 
+      this._psProc.stdin.end()
       console.log('Stopped listening')
-    });
+    })
   }
 }
 
-export default ProcessListener;
+export default ProcessListener
 
-// const listener = new ProcessListener();
+const listener = new ProcessListener()
 
-// listener.on('process-event', (data) => {
-//   console.log("Process event: " + data);
-// });
+// listener.on('process-event', (event) => {
+//   // console.log("Process event: " + event)
+//   console.log("Process event: " + JSON.stringify(event))
+// })
 
 // listener.on('powershell-error', (data) => {
-//   console.log("Powershell Error: " + data);
-// });
+//   console.log("Powershell Error: " + data)
+// })
 
 // setTimeout(() => {
 //   console.log('stopping listener')
-//   listener.stop();
-// }, 10000);
+//   listener.stop()
+// }, 10000)
--- src/powershell/register-events.ps1
+++ src/powershell/register-events.ps1
@@ -1,14 +1,25 @@
 param(
-    [Parameter(Mandatory=$true)][string]$StartEventIdentifier,
-    [Parameter(Mandatory=$true)][string]$StopEventIdentifier
+    [Parameter(Mandatory = $true)][string]$StartEventIdentifier,
+    [Parameter(Mandatory = $true)][string]$StopEventIdentifier
 )
+function global:Convert-EventToJson {
+    param($Type, $TraceEvent)
+    [hashtable]$eventObj = @{}
+    $eventObj.Type = $Type
+    $eventObj.ProcessID = $TraceEvent.ProcessID
+    $eventObj.ProcessName = $TraceEvent.ProcessName
+    $eventObj.TimeCreated = $TraceEvent.TIME_CREATED
+
+    $json = $eventObj | ConvertTo-Json -Compress
+    Write-Host $json
+}
 
 Register-WMIEvent -query "SELECT * FROM Win32_ProcessStartTrace" -SourceIdentifier "startevent" -action { 
-    $e = $Event.SourceEventArgs.NewEvent 
-    Write-Host $e.ProcessName,"started" 
+    $e = $Event.SourceEventArgs.NewEvent
+    global:Convert-EventToJson -Type startTrace -TraceEvent $e
 }
 
 Register-WMIEvent -query "SELECT * FROM Win32_ProcessStopTrace" -SourceIdentifier "stopevent" -action { 
-    $e = $Event.SourceEventArgs.NewEvent 
-    Write-Host $e.ProcessName,"stopped" 
+    $e = $Event.SourceEventArgs.NewEvent  
+    global:Convert-EventToJson -Type "stopTrace" -TraceEvent $e
 }
