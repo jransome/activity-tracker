3b95df2787b9f024304dcdeeababde4e38a31d86
James Ransome <jamesransome@msn.com>
2018-09-11 21:32:16 +0100

    delete redundant snapshot model

--- src/models/snapshot.js
+++
@@ -1,9 +0,0 @@
-export default (sequelize, DataTypes) => {
-  const Snapshot = sequelize.define('Snapshot', {
-    takenAt: DataTypes.DATE
-  }, {})
-  Snapshot.associate = (models) => {
-    // associations can be defined here
-  }
-  return Snapshot
-}
