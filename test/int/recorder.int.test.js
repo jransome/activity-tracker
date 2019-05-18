3ebd694835bc4ee44ab8fb934e3592e5ce32c10b
James Ransome <jamesransome@msn.com>
2019-05-18 22:20:37 +0300

    Update test

--- test/int/recorder.int.test.js
+++ test/int/recorder.int.test.js
@@ -10,13 +10,6 @@ describe('recording focus', () => {
   let mockPoller;
   let mockListener;
 
-  const mockPolled = {
-    path: 'C:/folder/program.exe',
-    pid: 1,
-    exeName: 'program.exe',
-    timestamp: new Date(),
-  }
-
   beforeEach(async () => {
     console.log('reinitialising test db...')
     dbConnection = await initDb(config)
@@ -30,53 +23,70 @@ describe('recording focus', () => {
   afterEach(async () => console.log('resetting test db...') || await databaseHelpers.reset(dbConnection))
 
   describe('saving initial snapshot', () => {
-    it('records the program that currently has focus', async () => {
+    it('records the program that has focus on startup', async () => {
       let enqueueFunction
       const internalQueueDrained = new Promise(res => {
         enqueueFunction = queueFactory(() => res())
       })
-      const recorder = recorderFactory(enqueueFunction)
-      mockPoller.mockResolvedValue(mockPolled)
+      const mockFocusEvents = [
+        {
+          path: 'C:/folder/program.exe',
+          pid: 1,
+          exeName: 'program.exe',
+          startTime: new Date(1000),
+        },
+        {
+          pid: 99,
+          path: 'some/path/theNextFocus.exe',
+          exeName: 'theNextFocus.exe',
+          startTime: new Date(2000)
+        }
+      ]
+      mockPoller.mockResolvedValue(mockFocusEvents[0])
+      const startRecorder = recorderFactory(enqueueFunction)
 
-      recorder.saveInitialFocus(dbConnection, mockPoller)
+      await startRecorder(dbConnection, mockPoller, mockListener)
+      mockListener.listener.emit('data', mockFocusEvents[1])
       await internalQueueDrained
 
       const { focusSessions, programs } = await databaseHelpers.getAllModels(dbConnection)
       expect(focusSessions.length).toEqual(1)
-      expect(focusSessions[0].exeName).toEqual(mockPolled.exeName)
+      expect(focusSessions[0].exeName).toEqual(mockFocusEvents[0].exeName)
+      expect(focusSessions[0].duration).toEqual(mockFocusEvents[1].startTime - mockFocusEvents[0].startTime)
       expect(programs.length).toEqual(1)
-      expect(programs[0].exeName).toEqual(mockPolled.exeName)
+      expect(programs[0].exeName).toEqual(mockFocusEvents[0].exeName)
+      expect(programs[0].focusTime).toEqual(focusSessions[0].duration)
       expect(programs[0]).toEqual(await focusSessions[0].getProgram())
     })
   })
 
-  describe('when focus change is detected', () => {
-    it('ends the focus session for the old program', async () => {
-      await databaseHelpers.createFakeSession(dbConnection, 'a.exe')
-      let enqueueFunction
-      const internalQueueDrained = new Promise(res => {
-        enqueueFunction = queueFactory(() => res())
-      })
-      const recorder = recorderFactory(enqueueFunction)
+  // describe('when focus change is detected', () => {
+  //   it('ends the focus session for the old program', async () => {
+  //     await databaseHelpers.createFakeSession(dbConnection, 'a.exe')
+  //     let enqueueFunction
+  //     const internalQueueDrained = new Promise(res => {
+  //       enqueueFunction = queueFactory(() => res())
+  //     })
+  //     const recorder = recorderFactory(enqueueFunction)
 
-      recorder.startRecorder(dbConnection, mockListener)
-      mockListener.listener.emit('data', {
-        pid: 99,
-        path: 'some/path/b.exe',
-        exeName: 'b.exe',
-        timestamp: new Date()
-      })
-      await internalQueueDrained
+  //     recorder.startRecorder(dbConnection, mockListener)
+  //     mockListener.listener.emit('data', {
+  //       pid: 99,
+  //       path: 'some/path/b.exe',
+  //       exeName: 'b.exe',
+  //       timestamp: new Date()
+  //     })
+  //     await internalQueueDrained
 
-      const { focusSessions, programs } = await databaseHelpers.getAllModels(dbConnection)
-      expect(focusSessions.length).toEqual(2)
-      expect(focusSessions[0].exeName).toEqual('a.exe')
-      expect(focusSessions[0].isActive).toBe(false)
-      expect(focusSessions[1].exeName).toEqual('b.exe')
-      expect(focusSessions[1].isActive).toBe(true)
-      expect(programs.length).toEqual(2)
-      expect((await focusSessions[0].getProgram()).exeName).toEqual('a.exe')
-      expect((await focusSessions[1].getProgram()).exeName).toEqual('b.exe')
-    })
-  })
+  //     const { focusSessions, programs } = await databaseHelpers.getAllModels(dbConnection)
+  //     expect(focusSessions.length).toEqual(2)
+  //     expect(focusSessions[0].exeName).toEqual('a.exe')
+  //     expect(focusSessions[0].isActive).toBe(false)
+  //     expect(focusSessions[1].exeName).toEqual('b.exe')
+  //     expect(focusSessions[1].isActive).toBe(true)
+  //     expect(programs.length).toEqual(2)
+  //     expect((await focusSessions[0].getProgram()).exeName).toEqual('a.exe')
+  //     expect((await focusSessions[1].getProgram()).exeName).toEqual('b.exe')
+  //   })
+  // })
 })
