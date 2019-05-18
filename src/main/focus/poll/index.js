bbc06cf2e6e894c63a3294cb58dd4cb564481ef8
James Ransome <jamesransome@msn.com>
2019-05-18 21:38:08 +0300

    Simplify recorder

--- src/main/focus/poll/index.js
+++ src/main/focus/poll/index.js
@@ -25,7 +25,6 @@ module.exports = () => Promise.race([pollTimeout, new Promise((resolve, reject) 
     try {
       const event = tryJsonParse(data)
       event.exeName = event.path.trim().split('/').slice(-1)[0]
-      event.timestamp = new Date()
       logger.debug(`Found current focus: ${event.exeName}`)
       await psProc.end()
       logger.debug('Ended powershell child process')
