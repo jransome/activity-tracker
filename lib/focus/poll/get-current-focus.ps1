43c1432720bb76270a0d562413e62d8b0f31fc38
James Ransome <jamesransome@msn.com>
2019-07-07 11:46:56 +0100

    Add retries on poll script

--- lib/focus/poll/get-current-focus.ps1
+++ lib/focus/poll/get-current-focus.ps1
@@ -1,21 +1,26 @@
-[CmdletBinding()]            
-Param(            
-)            
 Add-Type @"
   using System;
   using System.Runtime.InteropServices;
   public class FocusPoller {
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
+  }
+"@
+
+while (($null -eq $activeHandle) -and ($retries -lt 20)) {
+  $activeHandle = [FocusPoller]::GetForegroundWindow()
+  $retries++
 }
-"@  
 
-$activeHandle = [FocusPoller]::GetForegroundWindow()
-$process = Get-Process | Where-Object {$_.MainWindowHandle -eq $activeHandle}
+if ($null -ne $activeHandle) {
+  $process = Get-Process | Where-Object { $_.MainWindowHandle -eq $activeHandle }
+  [hashtable]$focusObj = @{ }
+  $focusObj.pid = $process.Id
+  $focusObj.path = $process.MainModule.FileName.Replace('\', '/')
+  $json = $focusObj | ConvertTo-Json -Compress
+}
+else {
+  $json = $null
+}
 
-[hashtable]$focusObj = @{}
-$focusObj.pid = $process.Id
-$focusObj.path = $process.MainModule.FileName.Replace('\', '/')
-
-$json = $focusObj | ConvertTo-Json -Compress
 Write-Host $json
